94d109b8dca83345dc586669ed12ebd773c81c47
Izzy van der Giessen <i.vandergiessen@ziggo.nl>
2023-04-06 15:32:25 +0200

    Finish part 2

--- day3/main.hs
+++ day3/main.hs
@@ -4,19 +4,25 @@ import Data.Char
 import Control.Monad
 
 
-magicFilter :: String -> Char
-magicFilter s =
-  let (left, right) = splitAt ((length s) `div` 2) s
-  in head $ filter (`elem` left) right
+magicFilter :: (String, String, String) -> Char
+magicFilter (one, two, three) =
+  head $ filter (`elem` one) $ filter (`elem` two) three
 
 charToScore :: Char -> Int
 charToScore c =
   if isUpper c then (ord c) - (ord 'A') + 27
   else (ord c) - (ord 'a') + 1
 
+groupElves :: [String] -> [(String, String, String)]
+groupElves [] = []
+groupElves (one:two:three:s) = (one, two, three) : groupElves s
+groupElves _ = error "Input not a multiple of three."
+
+
 main = do
   file <- openFile "input/input" ReadMode
   contents <- hGetContents file
-  let ans = sum $ map charToScore $ map magicFilter $ lines contents
+  let ans = sum $ map charToScore $ map magicFilter
+        $ groupElves $ lines contents
   print ans
   hClose file
