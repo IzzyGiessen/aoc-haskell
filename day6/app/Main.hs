b05e996d6b0302087516496fdb97a81898f3d0d6
Izzy van der Giessen <i.vandergiessen@ziggo.nl>
2023-04-07 14:40:10 +0200

    Finish part 2

--- day6/app/Main.hs
+++ day6/app/Main.hs
@@ -6,9 +6,9 @@ import Control.Monad
 findStart :: Int -> String -> Int
 findStart 0 _ = 0
 findStart n [] = error "There is no start of packet"
-findStart n s = let sig = take 4 s
+findStart n s = let sig = take n s
     in if sig == nub sig
-    then 4
+    then n
     else (+) 1 $ findStart n $ tail s
 
 main :: IO ()
@@ -16,6 +16,6 @@ main = do
   file <- openFile "input/input" ReadMode
   contents <- hGetContents file
   let ls = head $ lines contents
-  let start = findStart 4 ls
+  let start = findStart 14 ls
   print $ start
   hClose file
